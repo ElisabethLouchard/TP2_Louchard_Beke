efec9c63d39a7d2b7d47bc959e4015aed83e6f29
Elisabeth <elisabethlouchard@gmail.com>
2024-05-02 09:06:44 -0400

    Dédoublage d'import

--- tests/Feature/FilmTest.php
+++ tests/Feature/FilmTest.php
@@ -2,9 +2,6 @@
 
 namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use App\Models\Film;
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\Language;
